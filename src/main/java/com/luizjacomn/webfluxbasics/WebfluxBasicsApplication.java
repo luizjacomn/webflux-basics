14d64e7b974314555a7b1ecaffa588367288eec9
Luiz Jacó <luizjaco.dev@gmail.com>
2025-04-28 11:34:23 -0300

    Setuping reactive project

---
+++ src/main/java/com/luizjacomn/webfluxbasics/WebfluxBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.luizjacomn.webfluxbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizjacomn/webfluxbasics/WebfluxBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizjacomn.webfluxbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
